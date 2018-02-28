ea440c0342e5abedeca2d73f416ac88c1db94500
Lucas Teixeira Rocha <lucasrochabr@outlook.com>
2018-02-28 15:39:59 -0300

    Adds new tests

--- server/test/subject.js
+++ server/test/subject.js
@@ -1,8 +1,7 @@
 process.env.NODE_ENV = 'test';
 
-let mongoose = require('mongoose');
 let config = require('config');
-let Subject = require('../modules/subject/subject_model');
+let User = require('../modules/user/user_model');
 
 //Require the dev-dependencies
 let chai = require('chai');
@@ -12,21 +11,154 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 describe('Subjects', () => {
+    let token = '';
+    let testSubject = {
+        id: 'test',
+        name: 'test'
+    };
 
-  /*
-  * Test the /GET route
-  */
-  describe('/GET subject', () => {
-      it('it should GET an empty list of subjects', (done) => {
-        chai.request(server)
-            .get(config.apiRoot + '/subject')
+    before((done) => {
+        const newUser = new User({
+          username: 'test',
+          password: 'test'
+        });
+        // save the user
+        newUser.save((err) => {
+            if (!err) {
+                token = newUser.generateToken();
+            }
+            done(err);
+        });
+    });
+
+    describe('/POST subject', () => {
+        it('shouldn\'t create a subject without giving a token', (done) => {
+            chai.request(server)
+            .post(config.apiRoot + '/subject')
             .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(0);
+                res.should.have.status(401);
+                res.body.should.be.an('object');
                 done();
             });
-      });
-  });
+        });
+        it('shouldn\'t create an incomplete subject', (done) => {
+            chai.request(server)
+            .post(config.apiRoot + '/subject')
+            .set({'Authorization': token})
+            .send({
+                id: 'test',
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('success');
+                res.body.success.should.be.false;
+                done();
+            });
+        });
+        it('should create a new subject', (done) => {
+            chai.request(server)
+            .post(config.apiRoot + '/subject')
+            .set({'Authorization': token})
+            .send(testSubject)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('success');
+                res.body.success.should.be.true;
+                res.body.should.have.property('subject');
+                res.body.subject.should.be.a('object');
+                res.body.subject.should.have.property('id');
+                res.body.subject.id.should.be.equals(testSubject.id);
+                res.body.subject.should.have.property('name');
+                res.body.subject.name.should.be.equals(testSubject.name);
+                done();
+            });
+        });
+    });
+
+    describe('/GET subject', () => {
+        it('should GET list of subjects', (done) => {
+          chai.request(server)
+              .get(config.apiRoot + '/subject')
+              .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+                  res.body.length.should.be.equals(1);
+                  done();
+              });
+        });
+        it('should GET a subject', (done) => {
+            chai.request(server)
+            .get(config.apiRoot + '/subject/' + testSubject.id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('success');
+                res.body.success.should.be.true;
+                res.body.should.have.property('subject');
+                res.body.subject.should.be.a('object');
+                res.body.subject.should.have.property('id');
+                res.body.subject.id.should.be.equals(testSubject.id);
+                res.body.subject.should.have.property('name');
+                res.body.subject.name.should.be.equals(testSubject.name);
+                done();
+            });
+        });
+    });
+
+    describe('/PUT subject', () => {
+        const updatedSubject = { name: 'updated test' };
+        it('shouldn\'t update a subject without giving a token', (done) => {
+          chai.request(server)
+              .put(config.apiRoot + '/subject/' + testSubject.id)
+              .send(updatedSubject)
+              .end((err, res) => {
+                  res.should.have.status(401);
+                  done();
+              });
+        });
+        it('should update a subject', (done) => {
+            chai.request(server)
+            .put(config.apiRoot + '/subject/' + testSubject.id)
+            .set('Authorization', token)
+            .send(updatedSubject)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('success');
+                res.body.success.should.be.true;
+                res.body.should.have.property('subject');
+                res.body.subject.should.be.a('object');
+                res.body.subject.should.have.property('name');
+                res.body.subject.name.should.be.equals(updatedSubject.name);
+                done();
+            });
+        });
+    });
+
+    describe('/DELETE subject', () => {
+        const updatedSubject = { name: 'updated test' };
+        it('shouldn\'t delete a subject without giving a token', (done) => {
+          chai.request(server)
+              .delete(config.apiRoot + '/subject/' + testSubject.id)
+              .end((err, res) => {
+                  res.should.have.status(401);
+                  done();
+              });
+        });
+        it('should delete a subject', (done) => {
+            chai.request(server)
+            .delete(config.apiRoot + '/subject/' + testSubject.id)
+            .set('Authorization', token)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('success');
+                res.body.success.should.be.true;
+                done();
+            });
+        });
+    });
 
 });
--- server/test/user.js
+++ server/test/user.js
@@ -1,8 +1,5 @@
 process.env.NODE_ENV = 'test';
-
-let mongoose = require('mongoose');
 let config = require('config');
-let User = require('../modules/user/user_model');
 
 //Require the dev-dependencies
 let chai = require('chai');
@@ -22,7 +19,7 @@ describe('Users', () => {
   * Test the /signup route
   */
   describe('/POST signup', () => {
-    it('it should not create a user with no data', (done) => {
+    it('shouldn\'t create a user with no data', (done) => {
       chai.request(server)
         .post(config.apiRoot + '/signup')
         .send()
@@ -33,7 +30,7 @@ describe('Users', () => {
           done();
         });
     });
-    it('it should create a new user', (done) => {
+    it('should create a new user', (done) => {
       chai.request(server)
         .post(config.apiRoot + '/signup')
         .send(credentials)
@@ -41,6 +38,9 @@ describe('Users', () => {
           res.body.should.be.an('object');
           res.body.should.have.property('success');
           res.body.success.should.be.true;
+          res.body.should.have.property('token');
+          res.body.token.should.be.a('string');
+          credentials.token = res.body.token;
           done();
         });
     });
@@ -50,18 +50,33 @@ describe('Users', () => {
     * Test the /signin route
     */
     describe('/POST signin', () => {
-      it('it should authenticate a user', (done) => {
-        chai.request(server)
-          .post(config.apiRoot + '/signin')
-          .send(credentials)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.an('object');
-            res.body.should.have.property('success');
-            res.body.success.should.be.true;
-            done();
-          });
-      });
+        it('shouldn\'t authenticate a user with wrong credentials', (done) => {
+          chai.request(server)
+            .post(config.apiRoot + '/signin')
+            .send({
+                username: credentials.username,
+                password: 'Wrong Password'
+            })
+            .end((err, res) => {
+              res.should.have.status(401);
+              res.body.should.be.an('object');
+              res.body.should.have.property('success');
+              res.body.success.should.be.false;
+              done();
+            });
+        });
+        it('should authenticate a user with correct credentials', (done) => {
+          chai.request(server)
+            .post(config.apiRoot + '/signin')
+            .send(credentials)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.an('object');
+              res.body.should.have.property('success');
+              res.body.success.should.be.true;
+              done();
+            });
+        });
     });
 
 });
